20e0f879b3f3798274f451181237478eb4b65650
arjinmc <arjinmc@hotmail.com>
2018-03-19 17:28:33 +0800

    add InnerRecyclerView

---
+++ expandrecyclerview/src/main/java/com/arjinmc/expandrecyclerview/view/InnerRecyclerView.java
@@ -0,0 +1,34 @@
+package com.arjinmc.expandrecyclerview.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * InnerRecyclerView
+ * Created by Eminem Lo on 2018/3/19.
+ * email: arjinmc@hotmail.com
+ */
+
+public class InnerRecyclerView extends RecyclerView {
+
+    public InnerRecyclerView(Context context) {
+        super(context);
+    }
+
+    public InnerRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public InnerRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(
+                Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
